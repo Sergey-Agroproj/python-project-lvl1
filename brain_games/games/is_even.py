a477e2938a63a946ac2d9f235f6980aabaf5d130
Sergey Sukharev <agroproj@mail.ru>
2021-12-17 21:51:40 +0300

    fix is_even.py

--- brain_games/games/is_even.py
+++ brain_games/games/is_even.py
@@ -3,23 +3,29 @@ import random
 import prompt
 
 
-def is_even(name):
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+
+def even(name):
     print('Answer "yes" if the number is even, otherwise "no".')
     count = 0
     for _ in range(3):
-        req = random.randint(1, 100)
-        print('Question: {}'.format(req))
-        ans = prompt.string('Your answer: ')
-        if req % 2 == 0 and ans == 'yes' or req % 2 == 1 and ans == 'no':
+        request = random.randint(1, 100)
+        print('Question: {}'.format(request))
+        answer = prompt.string('Your answer: ')
+        if is_even(request) is True:
+            may_answer = 'yes'
+        else:
+            may_answer = 'no'
+        if answer == may_answer:
             print('Correct!')
             count += 1
         else:
-            if req % 2 == 0:
-                may_ans = 'yes'
-            if req % 2 == 1:
-                may_ans = 'no'
-            print("'{}' is wrong answer ;(.".format(ans), end=' ')
-            print("Correct answer was '{}'".format(may_ans))
+            print("'{}' is wrong answer ;(.".format(answer), end=' ')
+            print("Correct answer was '{}'".format(may_answer))
             print("Let's try again, {}!".format(name))
             break
     if count == 3:
